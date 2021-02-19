21cac65289365132bf4964dc791d2b0b9b4ef481
IsabelBender <isabelsbender@gmail.com>
2021-02-18 20:08:58 -0800

    init

---
+++ js/script.js
@@ -0,0 +1,181 @@
+// STEP 1
+
+/* let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction */
+
+// STEP 2
+
+/* let count = 1; 
+let name = "Isabel"; 
+let isValid = true; 
+let name = "null"; */ 
+
+// STEP 3
+/*
+// Complex
+let anExpression = 3 * (25 / 5) + 1; 
+
+// Variable
+let myName = "Isabel Bender"; 
+*/
+
+// STEP 4
+
+/*
+let firstName; 
+let lastName; 
+let myAddress; 
+let myCity;
+let myState; 
+let myZipCode; 
+let myAge; 
+let referralSource; 
+let mayWeContactYou; 
+*/
+
+// STEP 5
+
+/*
+let firstName; 
+firstName = "Isabel Bender";
+
+let myCity; 
+myCity = "Chula Vista"; 
+
+let myAge; 
+myAge = "28"; 
+*/
+
+// STEP 6
+
+/*
+let greeting = "Hello,  is the price really $";
+window.console.log(greeting +15.99 +"?");   
+*/
+
+// STEP 7
+
+/* let myName = "Isabel Bender";
+let isStudent = true; 
+alert(myName + " " + isStudent); */
+
+/* let yearsStudying = "4"; 
+let totalYears = "false"; 
+alert(("Isabel has been studying for " + yearsStudying + 2)); */
+
+// STEP 8
+
+/*
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);
+*/
+
+// STEP 9
+
+/*
+let val1 ="null";
+window.console.log(val1); 
+*/
+
+// window.console.log(val1);
+
+// STEP 10
+
+// window.console.log(typeof "Hello"); 
+// window.console.log(typeof 3); 
+// window.console.log(typeof true);
+// window.console.log(typeof &&); 
+// window.console.log(typeof undefined);  
+
+
+// STEP 11
+/*
+const greeting = "Hello "; 
+const name = "Isabel Bender"; 
+const welcome = ", welcome to the JavaScript class!";
+alert (greeting + name + welcome);
+*/
+
+// STEP 12 
+/*
+let greeting = "Hello "; 
+let name = "Isabel"; 
+let welcome = ", welcome to the JavaScript class!";
+alert (greeting + name + welcome); 
+*/
+
+// STEP 13
+/*
+let greeting = "Hello "; 
+let name = "Isabel"; 
+let welcome = ", welcome to the";
+let course = "Javascript"; 
+let class = "class!";
+alert (greeting + name + welcome + course + class); 
+*/
+
+// STEP 14
+/*
+let greeting = "Hello "; 
+let name = "Isabel"; 
+let welcome = ", welcome to the";
+let course = "Javascript";
+let class = "class!"; 
+alert (greeting + name\n + welcome + course + class);
+*/
+
+// STEP 15
+/*
+let userEntry= (window.prompt('Hello, what is your name?'));
+alert ('Welcome to the Javascript course' + userEntry);
+*/
+
+// STEP 16
+/*
+let userEntry= (window.prompt('Hello, what class are you taking?'));
+alert ('Welcome to the' + userEntry + 'course');
+/*
+
+// STEP 17
+
+/*
+let x = 10; 
+let y = 20; 
+let total = x + y; 
+window.console.log(total);
+*/ 
+
+// STEP 18 
+/*
+let x = 20; 
+window.console.log(x + x); 
+*/ 
+
+// STEP 19
+/*
+let x = 20; 
+window.console.log(x * 5); 
+*/ 
+
+// STEP 20
+/*
+let x = 2; 
+window.console.log(x/1); 
+*/ 
+
+// STEP 21
+/*
+let x = 5
+let y = 5 
+window.console.log(x == y && y == x); 
+*/ 
+
+// STEP 22
+/*
+let x = 3
+let y = 1
+window.console.log(y > x || x < y);
+*/
